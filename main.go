816248a214d489ea68c323159799d1a7533cb91e
Nikita Pralich <leroki13@gmail.com>
2017-09-23 17:47:32 +0800

    new features
    Добавлены комманды:
    - вывод шаблонов /showtemplates
    - добавление шаблона /addnewtemplate <ListName>  
    <ListElement> ...

--- main.go
+++ main.go
@@ -5,20 +5,27 @@ import (
 	"gopkg.in/telegram-bot-api.v4"
 	"os"
 	"encoding/json"
+	"io/ioutil"
+	"strings"
 )
 
 type Config struct {
 	TelegramBotToken string
 }
 
-type CheckListTemplate struct {
+type CheckList struct {
 	Name string		`json:"name"`
 	Items []string	`json:"items"`
+}
+
+type CheckListTemplate struct {
+	CheckLists []CheckList `json:"lists"`
 }
 
 func main() {
 	os.Mkdir("./AppData", os.ModePerm)
 	file, _ := os.Open("config.json")
+	defer file.Close()
 	decoder := json.NewDecoder(file)
 	configuration := Config{}
 	err := decoder.Decode(&configuration)
@@ -58,8 +65,11 @@ func main() {
 			reply = "Привет. Я телеграм-бот"
 		case "hello":
 			reply = "world"
-		case "newcheklisttemplate":
-			go TestMethod(update, bot)
+		case "showtemplates":
+			go ShowTemplates(update, bot)
+			continue
+		case "addnewtemplate":
+			go AddNewTemplate(update, bot)
 			continue
 		}
 
@@ -70,8 +80,66 @@ func main() {
 	}
 }
 
-func TestMethod(update tgbotapi.Update, bot *tgbotapi.BotAPI)  {
-	reply := "ну типа все работает"
+func ShowTemplates(update tgbotapi.Update, bot *tgbotapi.BotAPI)  {
+	file, _ := os.Open("AppData/template.json")
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	clt := CheckListTemplate{}
+	err := decoder.Decode(&clt)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	reply := ""
+
+	for i := range clt.CheckLists {
+		reply += clt.CheckLists[i].Name + ":\n"
+		for y := range clt.CheckLists[i].Items {
+			reply += clt.CheckLists[i].Items[y] + "\n"
+		}
+		reply += "\n"
+	}
+
+	if reply == "" {
+		reply = "Функция показа шаблонов"
+	}
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+	bot.Send(msg)
+}
+
+func AddNewTemplate(update tgbotapi.Update, bot *tgbotapi.BotAPI)  {
+	filePath := "AppData/template.json"
+	rawDataIn, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal("Cannot load settings:", err)
+	}
+
+	var templ CheckListTemplate
+	err = json.Unmarshal(rawDataIn, &templ)
+	if err != nil {
+		log.Fatal("Invalid settings format:", err)
+	}
+
+	arguments := strings.Split(update.Message.CommandArguments(), " ")
+
+	newList := CheckList{
+		Name:  arguments[0],
+		Items: arguments[1:],
+	}
+
+	templ.CheckLists = append(templ.CheckLists, newList)
+
+	rawDataOut, err := json.MarshalIndent(&templ, "", "  ")
+	if err != nil {
+		log.Fatal("JSON marshaling failed:", err)
+	}
+
+	err = ioutil.WriteFile(filePath, rawDataOut, 0)
+	if err != nil {
+		log.Fatal("Cannot write updated settings file:", err)
+	}
+
+	reply := "Функция добавления новых шаблонов"
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 	bot.Send(msg)
 }
