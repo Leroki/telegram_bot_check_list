e9f5d1531d1c44c8b6d51895767aa3c5d204f55c
Nikita Pralich <leroki13@gmail.com>
2017-09-30 22:32:41 +0800

    Add menu Правда оно пока не очень работоспособное, но есть возможность переходить по иерархии

--- main.go
+++ main.go
@@ -28,17 +28,21 @@ type CheckListTemplate struct {
 	CheckLists []CheckList `json:"lists"`
 }
 
+type User struct {
+	ID    int64
+	State string
+}
+
 func main() {
 	os.Mkdir("./AppData", os.ModePerm)
 	file, _ := os.Open("config.json")
-	defer file.Close()
 	decoder := json.NewDecoder(file)
 	configuration := Config{}
 	err := decoder.Decode(&configuration)
-
 	if err != nil {
 		log.Panic(err)
 	}
+	file.Close()
 
 	bot, err := tgbotapi.NewBotAPI(configuration.TelegramBotToken)
 	if err != nil {
@@ -54,37 +58,124 @@ func main() {
 
 	updates, err := bot.GetUpdatesChan(u)
 
+	var Users map[string]User
+	Users = make(map[string]User)
+
+	// главный цикл
 	for update := range updates {
-		// универсальный ответ на любое сообщение
-		reply := "Не знаю что сказать"
 		if update.Message == nil {
 			continue
 		}
 
+		if Users[update.Message.From.UserName].ID == 0 {
+			log.Println(Users[update.Message.From.UserName].ID)
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Main",
+			}
+		}
+
 		// логируем от кого какое сообщение пришло
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		// debug info
+		log.Println(Users[update.Message.From.UserName].State)
 
 		// свитч на обработку комманд
 		// комманда - сообщение, начинающееся с "/"
 		switch update.Message.Command() {
 		case "start":
-			reply = "Привет. Я телеграм-бот"
-		case "hello":
-			reply = "world"
-		case "showtemplates":
-			go ShowTemplates(update, bot)
-			continue
-		case "addnewtemplate":
-			go AddNewTemplate(update, bot)
-			continue
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет "+update.Message.From.UserName+"! Я телеграм бот.")
+			bot.Send(msg)
+		case "stop":
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Пока "+update.Message.From.UserName+"!")
+			bot.Send(msg)
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Stopped",
+			}
 		}
 
-		// создаем ответное сообщение
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
-		// отправляем
-		bot.Send(msg)
+		switch update.Message.Text {
+		case "В главное меню":
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Main",
+			}
+		case "Шаблоны":
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Templates",
+			}
+		case "Показать мои шаблоны":
+			go ShowTemplates(update, bot)
+			continue
+		case "Добавить новый шаблон":
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Add tmp",
+			}
+		case "Отмена":
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Templates",
+			}
+		}
+
+		switch Users[update.Message.From.UserName].State {
+		case "Main":
+			keyRow1 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Листы"))
+			keyRow2 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Шаблоны"))
+			keyboard := tgbotapi.NewReplyKeyboard(keyRow1, keyRow2)
+			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Вы в главном меню")
+			msg.ReplyMarkup = keyboard
+			bot.Send(msg)
+		case "Stopped":
+			var keys []tgbotapi.KeyboardButton
+			keys = append(keys, tgbotapi.NewKeyboardButton("В главное меню"))
+			keys = append(keys, tgbotapi.NewKeyboardButton("Что-то :)"))
+			keyboard := tgbotapi.NewReplyKeyboard(keys)
+			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Вы в подменю")
+			msg.ReplyMarkup = keyboard
+			bot.Send(msg)
+		case "Templates":
+			keyRow1 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Показать мои шаблоны"))
+			keyRow2 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Добавить новый шаблон"))
+			keyRow3 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("В главное меню"))
+			keyboard := tgbotapi.NewReplyKeyboard(keyRow1, keyRow2, keyRow3)
+			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Вы в меню шаблонов")
+			msg.ReplyMarkup = keyboard
+			bot.Send(msg)
+		case "Add tmp":
+			keyRow1 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Отмена"))
+			keyRow2 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("В главное меню"))
+			keyboard := tgbotapi.NewReplyKeyboard(keyRow1, keyRow2)
+			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Введите название шаблона")
+			msg.ReplyMarkup = keyboard
+			bot.Send(msg)
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Add tmp name",
+			}
+		case "Add tmp name":
+
+		case "Add tmp item":
+
+		}
 	}
 }
+
+/*func RemoveUser(u []User, name string) []User  {
+	var id int
+	id = -1
+	for i := range u {
+		if u[i].Name == name {
+			id = i
+			break
+		}
+	}
+
+	return append(u[:id], u[id + 1:]...)
+}*/
 
 func ShowTemplates(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	file, _ := os.Open("AppData/template.json")
@@ -101,10 +192,7 @@ func ShowTemplates(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
 	for i := range templ.CheckLists {
 		reply += templ.CheckLists[i].Name + ":\n"
 		for y := range templ.CheckLists[i].Items {
-			reply += templ.CheckLists[i].Items[y].Name +
-				" : " +
-				strconv.FormatBool(templ.CheckLists[i].Items[y].State) +
-				"\n"
+			reply += templ.CheckLists[i].Items[y].Name + "\n"
 		}
 		reply += "\n"
 	}
