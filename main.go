69d1944bf41288c66bb6183004eb08a4f883ae35
Nikita Pralich <leroki13@gmail.com>
2017-10-03 00:48:39 +0800

    Шаблоны, функция добавления и просмотра всего 2 функции, но скоро их будет больше :)

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
-	// "strconv"
-)
+	/* "strings" */ /* "strconv" */)
 
 type Config struct {
 	TelegramBotToken string
@@ -28,9 +26,19 @@ type CheckListTemplate struct {
 	CheckLists []CheckList `json:"lists"`
 }
 
+type CheckListUser struct {
+	CheckLists []CheckList `json:"lists"`
+}
+
 type User struct {
 	ID    int64
 	State string
+}
+
+type TransactData struct {
+	UserName string
+	Data     string
+	Command  string
 }
 
 func main() {
@@ -61,6 +69,9 @@ func main() {
 	var Users map[string]User
 	Users = make(map[string]User)
 
+	var TData chan TransactData = make(chan TransactData)
+	go DataBase(TData)
+
 	// главный цикл
 	for update := range updates {
 		if update.Message == nil {
@@ -84,7 +95,7 @@ func main() {
 		// комманда - сообщение, начинающееся с "/"
 		switch update.Message.Command() {
 		case "start":
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет "+update.Message.From.UserName+"! Я телеграм бот.")
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привет "+update.Message.From.FirstName+"! Я телеграм бот.")
 			bot.Send(msg)
 			InitUser(update.Message.From.UserName)
 		case "stop":
@@ -96,6 +107,7 @@ func main() {
 			}
 		}
 
+		// обработка кнопок
 		switch update.Message.Text {
 		case "В главное меню":
 			Users[update.Message.From.UserName] = User{
@@ -120,22 +132,27 @@ func main() {
 				ID:    update.Message.Chat.ID,
 				State: "Templates",
 			}
+		case "Завершить":
+			if Users[update.Message.From.UserName].State == "Add tmp item" {
+				TData <- TransactData{
+					UserName: update.Message.From.UserName,
+					Data:     "",
+					Command:  "save tmp data",
+				}
+				Users[update.Message.From.UserName] = User{
+					ID:    update.Message.Chat.ID,
+					State: "Templates",
+				}
+			}
 		}
 
+		// обработка положения в меню
 		switch Users[update.Message.From.UserName].State {
 		case "Main":
 			keyRow1 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Листы"))
 			keyRow2 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Шаблоны"))
 			keyboard := tgbotapi.NewReplyKeyboard(keyRow1, keyRow2)
 			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Вы в главном меню")
-			msg.ReplyMarkup = keyboard
-			bot.Send(msg)
-		case "Stopped":
-			var keys []tgbotapi.KeyboardButton
-			keys = append(keys, tgbotapi.NewKeyboardButton("В главное меню"))
-			keys = append(keys, tgbotapi.NewKeyboardButton("Что-то :)"))
-			keyboard := tgbotapi.NewReplyKeyboard(keys)
-			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Вы в подменю")
 			msg.ReplyMarkup = keyboard
 			bot.Send(msg)
 		case "Templates":
@@ -158,49 +175,69 @@ func main() {
 				State: "Add tmp name",
 			}
 		case "Add tmp name":
-
+			TData <- TransactData{
+				UserName: update.Message.From.UserName,
+				Data:     update.Message.Text,
+				Command:  "add name",
+			}
+			keyRow1 := tgbotapi.NewKeyboardButtonRow(tgbotapi.NewKeyboardButton("Завершить"))
+			keyboard := tgbotapi.NewReplyKeyboard(keyRow1)
+			msg := tgbotapi.NewMessage(Users[update.Message.From.UserName].ID, "Введите название элемента или нажмите кнопку завершить для формирования листа")
+			msg.ReplyMarkup = keyboard
+			bot.Send(msg)
+			Users[update.Message.From.UserName] = User{
+				ID:    update.Message.Chat.ID,
+				State: "Add tmp item",
+			}
 		case "Add tmp item":
-
+			TData <- TransactData{
+				UserName: update.Message.From.UserName,
+				Data:     update.Message.Text,
+				Command:  "add item",
+			}
 		}
 	}
 }
 
-/*func RemoveUser(u []User, name string) []User  {
-	var id int
-	id = -1
-	for i := range u {
-		if u[i].Name == name {
-			id = i
-			break
-		}
-	}
-
-	return append(u[:id], u[id + 1:]...)
-}*/
+/* func RemoveUser(u []User, name string) []User  {
+ *     var id int
+ *     id = -1
+ *     for i := range u {
+ *         if u[i].Name == name {
+ *             id = i
+ *             break
+ *         }
+ *     }
+ *
+ *     return append(u[:id], u[id + 1:]...)
+ * } */
 
 func ShowTemplates(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	file, _ := os.Open("AppData/" + update.Message.From.UserName + "tem.json")
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	templ := CheckListTemplate{}
-	err := decoder.Decode(&templ)
+	filePath := "AppData/" + update.Message.From.UserName + ".tem.json"
+	rawDataIn, err := ioutil.ReadFile(filePath)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal("Cannot load settings:", err)
 	}
 
-	reply := ""
-
-	for i := range templ.CheckLists {
-		reply += templ.CheckLists[i].Name + ":\n"
-		for y := range templ.CheckLists[i].Items {
-			reply += templ.CheckLists[i].Items[y].Name + "\n"
+	var reply string
+	if len(rawDataIn) == 0 {
+		reply = "У вас нет шаблонов, вы можете их добавить"
+	} else {
+		var templ CheckListTemplate
+		err = json.Unmarshal(rawDataIn, &templ)
+		if err != nil {
+			log.Fatal("Invalid settings format:", err)
 		}
-		reply += "\n"
+		reply = ""
+		for i := range templ.CheckLists {
+			reply += templ.CheckLists[i].Name + ":\n"
+			for y := range templ.CheckLists[i].Items {
+				reply += templ.CheckLists[i].Items[y].Name + "\n"
+			}
+			reply += "\n"
+		}
 	}
 
-	if reply == "" {
-		reply = "Функция показа шаблонов"
-	}
 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
 	bot.Send(msg)
 }
@@ -209,65 +246,119 @@ func InitUser(UserName string) {
 	file1 := "AppData/" + UserName + ".json"
 	os.Create(file1)
 	file2 := "AppData/" + UserName + ".tem.json"
-	filePath := "AppData/template.json"
+	os.Create(file2)
+}
 
-	rawData, err := ioutil.ReadFile(filePath)
-	if err != nil {
-		log.Fatal("Cannot load settings:", err)
-	}
+func DataBase(TData chan TransactData) {
+	var CL map[string]CheckList = make(map[string]CheckList)
+	for {
+		ldata := <-TData
+		switch ldata.Command {
+		case "add name":
+			CL[ldata.UserName] = CheckList{
+				Name: ldata.Data,
+			}
+		case "add item":
+			if len(CL[ldata.UserName].Items) == 0 {
+				CL[ldata.UserName] = CheckList{
+					Name: CL[ldata.UserName].Name,
+					Items: []Item{{
+						Name:  ldata.Data,
+						State: false,
+					}},
+				}
+			} else {
+				var lItems []Item
+				for i := 0; i < len(CL[ldata.UserName].Items); i++ {
+					lItems = append(lItems, CL[ldata.UserName].Items[i])
+				}
+				lItems = append(lItems, Item{
+					Name:  ldata.Data,
+					State: false,
+				})
+				CL[ldata.UserName] = CheckList{
+					Name:  CL[ldata.UserName].Name,
+					Items: lItems,
+				}
+			}
+		case "save tmp data":
+			filePath := "AppData/" + ldata.UserName + ".tem.json"
+			rawDataIn, err := ioutil.ReadFile(filePath)
+			if err != nil {
+				log.Fatal("Cannot load settings:", err)
+			}
 
-	err = ioutil.WriteFile(file2, rawData, 0664)
-	if err != nil {
-		log.Fatal("Cannot write updated settings file:", err)
+			var templ CheckListTemplate
+			if len(rawDataIn) == 0 {
+				templ.CheckLists = append(templ.CheckLists, CL[ldata.UserName])
+			} else {
+				err = json.Unmarshal(rawDataIn, &templ)
+				if err != nil {
+					log.Fatal("Invalid settings format:", err)
+				}
+				templ.CheckLists = append(templ.CheckLists, CL[ldata.UserName])
+			}
+
+			rawDataOut, err := json.MarshalIndent(&templ, "", "  ")
+			if err != nil {
+				log.Fatal("JSON marshaling failed:", err)
+			}
+
+			err = ioutil.WriteFile(filePath, rawDataOut, 0664)
+			if err != nil {
+				log.Fatal("Cannot write updated settings file:", err)
+			}
+			delete(CL, ldata.UserName)
+		}
 	}
 }
 
-func AddNewTemplate(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
-	reply := "Функция добавления новых шаблонов"
-	if update.Message.CommandArguments() == "" {
-		reply = "Нет аргументов"
-	} else {
-		filePath := "AppData/template.json"
-		rawDataIn, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			log.Fatal("Cannot load settings:", err)
-		}
-
-		var templ CheckListTemplate
-		err = json.Unmarshal(rawDataIn, &templ)
-		if err != nil {
-			log.Fatal("Invalid settings format:", err)
-		}
-
-		arguments := strings.Split(update.Message.CommandArguments(), " ")
-
-		var item []Item
-		for i := 1; i < len(arguments); i++ {
-			item = append(item, Item{
-				Name:  arguments[i],
-				State: false,
-			})
-
-		}
-
-		newList := CheckList{
-			Name:  arguments[0],
-			Items: item,
-		}
-
-		templ.CheckLists = append(templ.CheckLists, newList)
-
-		rawDataOut, err := json.MarshalIndent(&templ, "", "  ")
-		if err != nil {
-			log.Fatal("JSON marshaling failed:", err)
-		}
-
-		err = ioutil.WriteFile(filePath, rawDataOut, 0)
-		if err != nil {
-			log.Fatal("Cannot write updated settings file:", err)
-		}
-	}
-
-	msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
-	bot.Send(msg)
-}
+/* func AddNewTemplate(update tgbotapi.Update, bot *tgbotapi.BotAPI) {
+ *     reply := "Функция добавления новых шаблонов"
+ *     if update.Message.CommandArguments() == "" {
+ *         reply = "Нет аргументов"
+ *     } else {
+ *         filePath := "AppData/template.json"
+ *         rawDataIn, err := ioutil.ReadFile(filePath)
+ *         if err != nil {
+ *             log.Fatal("Cannot load settings:", err)
+ *         }
+ *
+ *         var templ CheckListTemplate
+ *         err = json.Unmarshal(rawDataIn, &templ)
+ *         if err != nil {
+ *             log.Fatal("Invalid settings format:", err)
+ *         }
+ *
+ *         arguments := strings.Split(update.Message.CommandArguments(), " ")
+ *
+ *         var item []Item
+ *         for i := 1; i < len(arguments); i++ {
+ *             item = append(item, Item{
+ *                 Name:  arguments[i],
+ *                 State: false,
+ *             })
+ *
+ *         }
+ *
+ *         newList := CheckList{
+ *             Name:  arguments[0],
+ *             Items: item,
+ *         }
+ *
+ *         templ.CheckLists = append(templ.CheckLists, newList)
+ *
+ *         rawDataOut, err := json.MarshalIndent(&templ, "", "  ")
+ *         if err != nil {
+ *             log.Fatal("JSON marshaling failed:", err)
+ *         }
+ *
+ *         err = ioutil.WriteFile(filePath, rawDataOut, 0)
+ *         if err != nil {
+ *             log.Fatal("Cannot write updated settings file:", err)
+ *         }
+ *     }
+ *
+ *     msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+ *     bot.Send(msg)
+ * } */
