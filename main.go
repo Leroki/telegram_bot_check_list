f02fe2ecd6581034d50141b0baa3ad7a19e8b57c
Nikita Pralich <leroki13@gmail.com>
2017-10-18 13:05:52 +0800

    asd

--- main.go
+++ main.go
@@ -71,11 +71,11 @@ func main() {
 	u := tg.NewUpdate(0)
 	u.Timeout = 60
 
-	/*updates, err := bot.GetUpdatesChan(u)*/
+	updates, err := bot.GetUpdatesChan(u)
 
 	// http
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	log.Println("lalalal")
+	/*updates := bot.ListenForWebhook("/" + bot.Token)
+	log.Println("lalalal")*/
 
 	var Users map[string]User = make(map[string]User)
 
